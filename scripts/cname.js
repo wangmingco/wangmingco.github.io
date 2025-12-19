7d167197a72c74ae75db083f2f2f512dd0c081a5
wangming <wangming.co@hotmail.com>
2025-12-19 09:48:46 +0800

    Update cname.js

--- scripts/cname.js
+++ scripts/cname.js
@@ -4,11 +4,11 @@ console.log("Arguments:", process.argv.slice(2));
 
 const cnameContent = 'blog.vc89.cn';
 
-const cnamePath = path.resolve(__dirname, '../public/CNAME');
-fs.writeFileSync(cnamePath, cnameContent);
+const cnamePath2 = path.resolve(__dirname, './CNAME');
+fs.writeFileSync(cnamePath2, cnameContent);
 
 const cnamePath1 = path.resolve(__dirname, '../CNAME');
 fs.writeFileSync(cnamePath1, cnameContent);
 
-const cnamePath2 = path.resolve(__dirname, './CNAME');
-fs.writeFileSync(cnamePath2, cnameContent);
+const cnamePath = path.resolve(__dirname, '../public/CNAME');
+fs.writeFileSync(cnamePath, cnameContent);
